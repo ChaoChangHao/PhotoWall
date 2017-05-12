5f43336348d87e5411cd4029923745ba3ff811f7
Chang Hao Chao <changhaochao@Changde-MacBook-Pro.local>
2017-05-12 17:20:39 +0800

    no message

---
+++ View/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  PhotoWall
+//
+//  Created by Chang Hao Chao on 2017/5/12.
+//  Copyright © 2017年 Chang Hao Chao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
